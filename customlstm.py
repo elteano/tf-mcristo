f0e8b0a49cf4dcbf78f3a9c7a91b371554a3986e
Thomas Tucker <elteano@gmail.com>
2018-07-30 09:46:53 -0700

    Gradients are zero.

---
+++ customlstm.py
@@ -0,0 +1,58 @@
+# Because the library one is being mean.
+
+import tensorflow as tf
+import numpy as np
+
+class Lstm(object):
+    """ LSTM implementation based on
+    https://en.wikipedia.org/wiki/Long_short-term_memory#LSTM_with_a_forget_gate
+    """
+    def __init__(self, num_units, input_shape):
+        self.__num_units = num_units
+        self.__input_elems = input_shape.num_elements()
+        self.__scope = tf.name_scope('lstm')
+        # Input weight initialization
+        with self.__scope:
+            self.__Wf = self.__gen_input_weight(name='Wf')
+            self.__Wi = self.__gen_input_weight(name='Wi')
+            self.__Wo = self.__gen_input_weight(name='Wo')
+            self.__Wc = self.__gen_input_weight(name='Wc')
+            # Recurrent weight initialization
+            self.__Uf = self.__gen_rec_weight(name='Uf')
+            self.__Ui = self.__gen_rec_weight(name='Ui')
+            self.__Uo = self.__gen_rec_weight(name='Uo')
+            self.__Uc = self.__gen_rec_weight(name='Uc')
+            # Bias initialization
+            self.__Bf = self.__gen_bias(name='Bf')
+            self.__Bi = self.__gen_bias(name='Bi')
+            self.__Bo = self.__gen_bias(name='Bo')
+            self.__Bc = self.__gen_bias(name='Bc')
+
+    def __gen_input_weight(self, name=None):
+        return tf.Variable(np.random.rand(self.__num_units, self.__input_elems), dtype=tf.float32, name=name)
+
+    def __gen_rec_weight(self, name=None):
+        return tf.Variable(np.random.rand(self.__num_units, self.__num_units), dtype=tf.float32, name=name)
+
+    def __gen_bias(self, name=None):
+        return tf.Variable(np.zeros((self.__num_units, 1)), dtype=tf.float32, name=name)
+
+    def call(self, input, prev_out, prev_state):
+        with self.__scope:
+            with tf.name_scope('call'):
+                x = tf.reshape(input, (self.__input_elems, 1), name='Input_reshape')
+                f = tf.sigmoid(tf.add_n([tf.matmul(self.__Wf, x, name='Wf_matmul'), tf.matmul(self.__Uf, prev_out, name='Uf_matmul'), self.__Bf], name='forget_sum'), name='forget_sigmoid')
+                i = tf.sigmoid(tf.add_n([tf.matmul(self.__Wi, x, name='Wi_matmul'), tf.matmul(self.__Ui, prev_out, name='Ui_matmul'), self.__Bi], name='i_sum'), name='i_sigmoid')
+                o = tf.sigmoid(tf.add_n([tf.matmul(self.__Wo, x, name='Wo_matmul'), tf.matmul(self.__Uo, prev_out, name='Uo_matmul'), self.__Bo], name='o_sum'), name='o_sigmoid')
+                c_input = tf.tanh(tf.add_n([tf.matmul(self.__Wc, x, name='Wc_matmul'), tf.matmul(self.__Uc, prev_out, name='Uc_matmul'), self.__Bc], name='c_input_sum'), name='c_input_tanh')
+                c = tf.add_n([tf.multiply(f, prev_state, name='prev_state_multiply'), tf.multiply(i, c_input)], name='c_final_sum')
+                h = tf.multiply(o, tf.tanh(c, name='c_tanh'), name='hidden_output')
+                return o, c, h
+
+    @property
+    def weights(self):
+        return (self.__Wf)
+
+    def zero_state(self):
+        return (np.zeros((self.__num_units, 1), dtype=np.float32),
+            np.zeros((self.__num_units, 1), dtype=np.float32))
---
+++ groundup.py
@@ -0,0 +1,149 @@
+# New Monte Cristo generator I'm building from Tensorflow.
+
+import numpy as np
+import tensorflow as tf
+import os
+import pickle
+import customlstm
+from customlstm import Lstm
+from tensorflow.python import debug as tf_debug
+
+batch_size = 2
+num_units = 256
+num_epochs = 20
+mat_fname = 'cmonte.npy'
+p_fname = 'cmonte.dat'
+learn_rate = 0.3
+
+def oh_encode(arr, num_bins=None, num_rows=None):
+    if not num_bins:
+        num_bins = max(arr)
+    if not num_rows:
+        num_rows = len(arr)
+    mat = np.zeros((num_rows, num_bins), dtype=np.float32)
+    for i, v in enumerate(arr):
+        mat[i,v-1] = 1
+    return mat
+
+# Read and process the text
+if (os.path.isfile(mat_fname) and os.path.isfile(p_fname)):
+    oh_text = np.load(mat_fname)
+    with open (p_fname, 'rb') as pf:
+        chars, uchars, text_len, m_text = pickle.load(pf)
+else:
+    raw_text = ''
+    print('Reading file.')
+    with open('countmonte.txt', encoding='utf8') as f:
+        raw_text = f.read().lower()
+
+    print('Processing file.')
+    chars = sorted(list(set(raw_text)))
+    uchars = len(chars)
+    print('Number of unique characters: ' + str(uchars))
+    char_to_int = {c : i for i, c in enumerate(chars)}
+    int_to_char = {i : c for i, c in enumerate(chars)}
+
+    text_len = len(raw_text)
+
+    m_text = [char_to_int[c] for c in raw_text]
+    del raw_text
+    oh_text = oh_encode(m_text, num_bins = uchars)
+    np.save(mat_fname, oh_text)
+    with open(p_fname, 'wb') as pf:
+        pickle.dump((chars, uchars, text_len, m_text), pf)
+
+num_batches = (text_len // batch_size) * (batch_size)
+dataIn = []
+dataOut = []
+for i in range(text_len - batch_size):
+    seq_in = m_text[i:i+batch_size]
+    #seq_out = oh_text[i+batch_size:i+batch_size+1,:]
+    seq_out = m_text[i+batch_size]
+    dataIn.append(seq_in)
+    dataOut.append(seq_out)
+
+n_patterns = len(dataIn)
+del m_text
+
+print('Number of patterns: {:}'.format(n_patterns))
+
+print('Formatting data...')
+# Here we have the extra dimension so that unstack results in shapes of (1,1)
+procIn = np.reshape(dataIn, (n_patterns, batch_size, 1, 1))
+procIn = procIn / float(uchars)
+#procOut = oh_encode(dataOut, num_bins=uchars)
+procOut = dataOut
+print('Data formatted.')
+
+
+print('Preparing model')
+inp = tf.placeholder(tf.float32, [batch_size, 1, 1], name='Input_placeholder')
+otp = tf.placeholder(tf.int32, [1], name='Output_placeholder')
+st_c = tf.placeholder(tf.float32, [num_units, 1], name='c_state_inp')
+st_m = tf.placeholder(tf.float32, [num_units, 1], name='m_state_inp')
+
+#us_inp = list(map(lambda a : tf.reshape(a, (1,1)), tf.unstack(inp)))
+us_inp = tf.unstack(inp)
+
+lstm = Lstm(num_units, us_inp[0].get_shape())
+
+Ws = tf.Variable(np.random.rand(num_units, uchars), dtype=tf.float32, name='Out_categorize')
+bs = tf.Variable(np.zeros((1, uchars)), dtype=tf.float32, name='Categorize_bias')
+
+#lstm.build(us_inp[0].get_shape())
+
+st_ci, st_mi = st_c, st_m
+o = []
+st_i = tf.contrib.rnn.LSTMStateTuple(c=st_c, h=st_m)
+for i in range(batch_size):
+    o_i, st_ci, st_mi = lstm.call(us_inp[i], st_ci, st_mi)
+
+o_i = tf.add(tf.matmul(tf.transpose(o_i), Ws, name='out_categorization'), bs, name='out_cat_add_bias')
+    #o_i = tf.nn.softmax(tf.matmul(tf.transpose(o_i), Ws) + bs)
+    #o.append(o_i)
+o = o_i
+sm = tf.nn.softmax(o, name='test_softmax')
+
+#o = tf.concat(o, 0)
+loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=otp, logits=o)
+losses = tf.reduce_mean(loss, name='loss_mean')
+print('Losses is {:}'.format(losses))
+optimizer = tf.train.AdagradOptimizer(learn_rate)
+train_step = optimizer.compute_gradients(losses)
+apply_step = optimizer.apply_gradients(train_step, name='apply_gradients')
+
+mergesummary = tf.summary.merge_all()
+
+z_c, z_m = lstm.zero_state()
+print("Shapes: {:}, {:}".format(z_c.shape, z_m.shape))
+
+print('Beginning the session.')
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+    writer = tf.summary.FileWriter('log/', sess.graph)
+    for ep in range(num_epochs):
+        print('Starting epoch {:}'.format(ep))
+        run_loss = []
+        weights = []
+        for bat in range(num_batches):
+            _train_step, _loss, _apply_step = sess.run([train_step, losses, apply_step], feed_dict={
+            st_c:z_c, st_m:z_m, inp:procIn[bat], otp:[procOut[bat]]
+            })
+            weights.append(sess.run(lstm.weights))
+            run_loss.append(_loss)
+            if len(weights) > 1:
+                if (weights[-2] == weights[-1]).all():
+                    print('-2 is equal to -1')
+            #if bat > 0 and bat % 1000 == 0:
+            if bat == 5:
+                exit()
+            if True:
+                aloss = np.median(run_loss)
+                minloss = np.min(run_loss)
+                maxloss = np.max(run_loss)
+                print('Loss at batch {:} of {:}: {:} ({:} to {:})'.format(bat, num_batches, aloss, minloss, maxloss))
+                run_loss = []
+                #print('Output at batch {:}: {:}'.format(bat, _o[-1]))
+                #print('Expected at batch {:}: {:}'.format(bat, procOut[bat][-1]))
+    writer.close()
